eb8f68c7abf93a6e1e38cccafae5beb4e7fefd95
Mauricio Tollin <mauricio.tollin@gmail.com>
2016-05-03 23:29:09 -0300

    Creating Singleton class

---
+++ MySingletonExample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MySingletonExample : Singleton<MySingletonExample> {
+	protected MySingletonExample () {} // guarantee this will be always a singleton only - can't use the constructor!
+
+	public string globalVar;
+
+	void Awake () {
+		Debug.Log("Awoke Singleton Instance: " + gameObject.GetInstanceID());
+	}
+
+}
